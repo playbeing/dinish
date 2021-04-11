96ab2b1fc0404f11e6fa7cd0cc0700c64668f21a
Bert Driehuis <driehuis@playbeing.org>
2021-04-12 00:37:45 +0200

    Split font generation code into separate file to allow for suppressing noisy output

---
+++ process-font.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+in="$1"
+out="$2"
+
+die() {
+	echo "$@"
+	exit 1
+}
+
+res="`fontforge -c 'open(argv[1]).generate(argv[2])' $in $out 2>&1`"
+rv=$?
+echo "$res" | grep -Ev '(See AUTHORS|License GPLv3|with many parts BSD|Version: 201|Based on sources)' || true
+[ "$rv" = "0" ] || die "fontforge failed"
+
+case "$2" in
+	*.woff|*.woff2)
+		exit 0;;
+esac
+
+res="`gftools fix-unwanted-tables $out 2>&1`"
+rv=$?
+echo "$res" | grep -Ev '(since they are not in the font)' || true
+[ "$rv" = "0" ] || die "gftools fix-unwanted-tables failed"
+gftools fix-dsig --autofix $out
+rv=$?
+[ "$rv" = "0" ] || die "gftools fix-dsig failed"
