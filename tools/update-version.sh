305b7e11818fff2bb5fa795d6a42989f23eda26c
Bert Driehuis <driehuis@playbeing.org>
2021-08-15 17:47:09 +0200

    Update the script to take the version from the git tag. Also, stick in a "dev" suffix if not built from the release tag

--- tools/update-version.sh
+++ tools/update-version.sh
@@ -1,18 +1,42 @@
 #!/bin/sh
 
-major=$1
-minor=$2
+# Git tag looks like v2.006-29-g44c73e7
+# We first split on dash:
+read tag revs hash <<EOD
+`git describe --tags --long|tr - ' '`
+EOD
+# Remove the leading 'g' from the hash:
+hash=`echo $hash | sed -e s/^g//`
+modified=`git status -s -uno |grep -v ' fonts/'|wc -l`
+
+read major minor <<EOD
+`echo $tag|tr -d Vv|tr . ' '`
+EOD
+major=`printf %d $major`
+minor=`printf %d $minor`
 case $major:$minor in
 	[0-9]*:[0-9]*)
 		;;
 	*)
-		echo "Usage: $0 major minor"
+		echo "Malformed version string in git tag $tag; please fix before publishing; aborting!"
 		exit 1
 	;;
 esac
+
+if [ $modified = 0 ] && [ $revs = 0 ]; then
+    status=release
+else
+    if [ $modified -gt 0 ]; then
+        hash="$hash+$modified"
+    fi
+    status=dev
+fi
 version=`printf "%d.%03d" $major $minor`
+versionstr="Version $version; git-$hash-$status"
+
+echo $versionstr
 
 for ufo in sources/Dinish*/Dinish*.ufo
 do
-	sed -i -e "/versionMajor/,+1s/>[0-9]*</>$major</" -e "/versionMinor/,+1s/>[0-9]*</>$minor</" -e "/openTypeNameVersion/,+1s/>Version [0-9.]*</>Version $version</" $ufo/fontinfo.plist
+	sed -i -e "/versionMajor/,+1s/>[0-9]*</>$major</" -e "/versionMinor/,+1s/>[0-9]*</>$minor</" -e "/openTypeNameVersion/,+1s/>Version [0-9.]*</>$versionstr</" $ufo/fontinfo.plist
 done
