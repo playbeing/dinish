079bb33395661364863c109f16fa2a13bad38a98
Bert Driehuis <driehuis@playbeing.org>
2021-08-24 12:43:42 +0200

    Add a bit of explanation. Stick more info in version for unreproducable builds. Set the build date/time in openTypeHeadCreated

--- tools/update-version.sh
+++ tools/update-version.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+# Update the fontinfo.plist for all fonts:
+#   Version major/minor is set to the version from the git tag, e.g. v2.006
+#
+#   openTypeNameVersion is set to Version: tag; git-hash-release
+#   (e.g., Version: v2.006; git-1a2b3c4d-release) for releases built from
+#   the git tag, or to Version: tag; git-hash+revs-mods-dev for dev
+#   builds, e.g., Version 2.006; git-3ccbb4c+36-13-dev .
+#   The +36 indicates that this build is 36 commits ahead of 2.006.
+#   The -13 indicates that there are uncommitted changes to 13
+#   files, and that the build is not reproducable.
+#
+#   openTypeHeadCreated is set to the build date and time.
+
+
 # Git tag looks like v2.006-29-g44c73e7
 # We first split on dash:
 read tag revs hash <<EOD
@@ -26,17 +40,19 @@ esac
 if [ $modified = 0 ] && [ $revs = 0 ]; then
     status=release
 else
-    if [ $modified -gt 0 ]; then
-        hash="$hash+$modified"
-    fi
+    hash="$hash+$revs-$modified"
     status=dev
 fi
 version=`printf "%d.%03d" $major $minor`
 versionstr="Version $version; git-$hash-$status"
+now=`date +'%Y/%m/%d %H:%M:%S'`
 
 echo $versionstr
 
 for ufo in sources/Dinish*/Dinish*.ufo
 do
-	sed -i -e "/versionMajor/,+1s/>[0-9]*</>$major</" -e "/versionMinor/,+1s/>[0-9]*</>$minor</" -e "/openTypeNameVersion/,+1s/>Version [0-9.]*</>$versionstr</" $ufo/fontinfo.plist
+	sed -i -e "/versionMajor/,+1s/>[0-9]*</>$major</" \
+        -e "/versionMinor/,+1s/>[0-9]*</>$minor</" \
+        -e "/openTypeHeadCreated/,+1s#>[0-9].*<#>$now<#" \
+        -e "/openTypeNameVersion/,+1s/>Version [0-9.]*</>$versionstr</" $ufo/fontinfo.plist
 done
