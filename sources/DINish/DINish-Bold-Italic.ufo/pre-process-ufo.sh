128413c9aa2daedd906816a2fa6c06d32c265f01
Bert Driehuis <driehuis@playbeing.org>
2024-07-09 00:07:10 +0200

    Add DINish-Bold-Italic

---
+++ sources/DINish/DINish-Bold-Italic.ufo/pre-process-ufo.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+orig="$1"
+echo -n 'Preprocessing in '
+pwd
+
+[ -e glyphs ] || mkdir glyphs
+cp -n "$orig/sources/DINish/DINish-Bold.ufo/"*.* .
+cp -n "$orig/sources/DINish/DINish-Bold.ufo/glyphs/"*.* glyphs
+python "$orig"/tools/copy-missing-italics.py \
+  --source="$orig/sources/DINish/DINish-Bold.ufo" --dest=. \
+  --uprights="$orig"/sources/upright-in-italic-dinish.enc
