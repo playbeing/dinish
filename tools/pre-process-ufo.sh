26ec136de749387b7f611fa81e9950f5cbc6f880
Bert Driehuis <driehuis@playbeing.org>
2024-07-17 17:55:27 +0200

    Parametrize

--- tools/pre-process-ufo.sh
+++ tools/pre-process-ufo.sh
@@ -9,20 +9,23 @@ die() {
 	exit 1
 }
 
-[ -e tools ] || die "Run this script from the toplevel directory as ./tools/pre-process-ufo.sh"
+[ -e tools ] || die "Run this script from the toplevel directory as ./tools/pre-process-ufo.sh DINishExpanded"
+
+family=$1; shift
+[ -d sources/$family/$family-Bold.ufo ] || die "Source not found, did you pass the family name and just the family name?"
 
 topdir="`pwd`"
 tempdir=`mktemp -d "/tmp/process-font-XXXXXXXX" 2>/dev/null`
 cd $tempdir
-fontname=DINish-BoldItalic.ufo
+fontname=$family-BoldItalic.ufo
 mkdir $fontname
 cd $fontname
 
 [ -e glyphs ] || mkdir glyphs
-cp -n "$topdir/sources/DINish/DINish-Bold.ufo/"*.* .
-cp -n "$topdir/sources/DINish/DINish-Bold.ufo/glyphs/"*.* glyphs
+cp -n "$topdir/sources/$family/$family-Bold.ufo/"*.* .
+cp -n "$topdir/sources/$family/$family-Bold.ufo/glyphs/"*.* glyphs
 python "$topdir"/tools/copy-missing-italics.py \
-  --source="$topdir/sources/DINish/DINish-Bold.ufo" --dest=. \
+  --source="$topdir/sources/$family/$family-Bold.ufo" --dest=. \
   --uprights="$topdir"/sources/upright-in-italic-dinish.enc \
   --overwrite="a=a.ss02"
 
