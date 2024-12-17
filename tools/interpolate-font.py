54f71c08aec11941e33d3bb03c0fd26eec7bdadb
Bert Driehuis <driehuis@playbeing.org>
2024-12-17 19:37:00 +0100

    Better diagnostics

--- tools/interpolate-font.py
+++ tools/interpolate-font.py
@@ -93,7 +93,7 @@ def interpolate_font(src1, src2, dest, weight):
     # done!
     newfont.save(dest)
 
-    print("{0} incompatibilities seen".format(incompatibilities))
+    print("{0} incompatibilities seen between {1} and {2}".format(incompatibilities, src1, src2))
     if incompatibilities > 0:
         sys.exit(1)
 
