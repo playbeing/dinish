e1be6054d20cef21393e3459c16dda65ecd07b44
Bert Driehuis <driehuis@playbeing.org>
2024-12-30 23:29:34 +0100

    Include the 900 weight. For now, skip the CFF variable font.

--- tools/build-ephemeral-ufos.sh
+++ tools/build-ephemeral-ufos.sh
@@ -24,6 +24,7 @@ case "$version" in
     ;;
 esac
 
+make clean
 cd $scratch/sources
 mkdir vfwork
 
@@ -44,8 +45,24 @@ for f in DINish DINishCondensed DINishExpanded
 do
     mkdir -p $scratch/sources/vfwork/$f
     cp -r $f/$f-Regular.ufo $scratch/sources/vfwork/$f/$f-Regular.ufo
+    cp -r $f/$f-Bold.ufo $scratch/sources/vfwork/$f/$f-Bold.ufo
     python $toolsdir/interpolate-font.py --dest=$scratch/sources/vfwork/$f/$f-Heavy.ufo --weight=8 $f/$f-{Regular,Bold}.ufo
-    for weight in Regular Heavy
+    python $toolsdir/interpolate-font.py --dest=$scratch/sources/vfwork/$f/$f-Black.ufo --weight=9 $f/$f-{Regular,Bold}.ufo
+    # Fixup Heavy Condensed
+    if [ $f = DINishCondensed ]; then
+        for name in zero one two three four five six seven eight nine
+        do
+           cp $scratch/sources/vfwork/$f/$f-Bold.ufo/glyphs/$name.glif $scratch/sources/vfwork/$f/$f-Heavy.ufo/glyphs/
+           cp $scratch/sources/vfwork/$f/$f-Bold.ufo/glyphs/$name.glif $scratch/sources/vfwork/$f/$f-Black.ufo/glyphs/
+        done
+    fi
+    # Fixup Black
+    for name in ordfeminine ordmasculine zero.dnom one.dnom two.dnom three.dnom four.dnom five.dnom six.dnom seven.dnom eight.dnom nine.dnom
+    do
+       cp $scratch/sources/vfwork/$f/$f-Heavy.ufo/glyphs/$name.glif $scratch/sources/vfwork/$f/$f-Black.ufo/glyphs/
+    done
+
+    for weight in Regular Bold Black
     do
         if [ "$weight" = "Regular" ]; then
             stylename="Italic"
@@ -69,11 +86,13 @@ do
     python $toolsdir/interpolate-font.py --dest=$scratch/sources/$f/$f-Heavy.ufo --weight=8 $f/$f-{Regular,Bold}.ufo
     python $toolsdir/interpolate-font.py --dest=$scratch/sources/$f/$f-Black.ufo --weight=9 $f/$f-{Regular,Bold}.ufo
     # Fixup Heavy Condensed
-    for name in zero one two three four five six seven eight nine
-    do
-       cp $scratch/sources/$f/$f-Bold.ufo/glyphs/$name.glif $scratch/sources/$f/$f-Heavy.ufo/glyphs/
-       cp $scratch/sources/$f/$f-Bold.ufo/glyphs/$name.glif $scratch/sources/$f/$f-Black.ufo/glyphs/
-    done
+    if [ $f = DINishCondensed ]; then
+        for name in zero one two three four five six seven eight nine
+        do
+           cp $scratch/sources/$f/$f-Bold.ufo/glyphs/$name.glif $scratch/sources/$f/$f-Heavy.ufo/glyphs/
+           cp $scratch/sources/$f/$f-Bold.ufo/glyphs/$name.glif $scratch/sources/$f/$f-Black.ufo/glyphs/
+        done
+    fi
     # Fixup Black
     for name in ordfeminine ordmasculine zero.dnom one.dnom two.dnom three.dnom four.dnom five.dnom six.dnom seven.dnom eight.dnom nine.dnom
     do
@@ -116,13 +135,13 @@ python $toolsdir/nuke-inconsistent-anchors.py $scratch/sources/vfwork/DINish*/DI
 sed -i -e 's/\(DINish[A-Za-z]*\).Regular/\1/' $scratch/sources/vfwork/DINish*/DINish*-Regular.ufo/fontinfo.plist
 
 # Move this to the Makefile...
-fontmake --flatten-components DINish-Variable.designspace -o variable-cff2
-statmake --stylespace DINish-Variable.stylespace --designspace DINish-Variable.designspace variable_otf/DINish-Variable-VF.otf
+#fontmake --flatten-components DINish-Variable.designspace -o variable-cff2
+#statmake --stylespace DINish-Variable.stylespace --designspace DINish-Variable.designspace variable_otf/DINish-Variable-VF.otf
 fontmake --flatten-components DINish-Variable.designspace -o variable
 statmake --stylespace DINish-Variable.stylespace --designspace DINish-Variable.designspace variable_ttf/DINish-Variable-VF.ttf
 woff2_compress variable_ttf/DINish-Variable-VF.ttf
 mkdir -p fonts/ttf/variable fonts/otf/variable fonts/woff2/variable
 cp variable_ttf/DINish-Variable-VF.ttf fonts/ttf/variable/DINish$preview'[slnt,wdth,wght]'.ttf
 cp variable_ttf/DINish-Variable-VF.woff2 fonts/woff2/variable/DINish$preview'[slnt,wdth,wght]'.woff2
-cp variable_otf/DINish-Variable-VF.otf fonts/otf/variable/DINish$preview'[slnt,wdth,wght]'.otf
+#cp variable_otf/DINish-Variable-VF.otf fonts/otf/variable/DINish$preview'[slnt,wdth,wght]'.otf
 make all fontbakery
